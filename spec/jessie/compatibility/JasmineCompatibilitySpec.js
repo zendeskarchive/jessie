e85dfe36f06e190540287f6adc53e87ab9ff915b
Marcin Bunsch <marcin@applicake.com>
2011-06-11 11:37:19 +0200

    Unified stacktrace cleanup to happen in the reporter, removing an extension of Jasmine

--- spec/jessie/compatibility/JasmineCompatibilitySpec.js
+++ spec/jessie/compatibility/JasmineCompatibilitySpec.js
@@ -1,7 +1,7 @@
 describe('Jasmine Compabibility', function() {
 
   it("should run this spec", function() {
-
+    expect(1).toEqual(1)
   })
 
 });
--- spec/jessie/jasmine_spec.js
+++ spec/jessie/jasmine_spec.js
@@ -1,9 +1,5 @@
 describe('jessie.jasmine', function() {
 
-  it('should extend jasmine.Spec.prototype.fail with stacktrace', function() {
-    jasmine.Spec.prototype.fail.toString().should_match('expectationResult.stacktrace')
-  })
-
   it('should extend jasmine.PrettyPrinter.prototype.iterateObject with checking for should_ properties', function() {
     jasmine.PrettyPrinter.prototype.iterateObject.toString().should_match('var should_regex')
   })
