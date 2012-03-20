f34fc847bf4c31b6344ae5ddf41dd74180e5ba7a
Marcin Bunsch <marcin@futuresimple.com>
2012-03-20 18:00:11 +0100

    Adding callbacks and bumping to 0.4.2

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -3,6 +3,10 @@ jessie.sugar()
 
 require('coffee-script');
 
+jessie.callbacks.bind('finish', function() {
+  console.log('Finished!')
+});
+
 // Custom matcher
 beforeEach(function(){
   this.addMatchers({
